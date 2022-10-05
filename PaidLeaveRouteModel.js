53996fcf8a2ec3096880effc593c79d862fbc03b
yukimasaki <woodenhoe@gmail.com>
2022-10-05 05:16:25 +0000

    getRoutes関数をPaidLeaveRouteModel.gsへ移動

--- PaidLeaveRouteModel.js
+++ PaidLeaveRouteModel.js
@@ -1,5 +1,16 @@
-// role=承認のレコード数を取得
+function getRoutes(department) {
+  // 申請ルートをオブジェクトとして取得
+  const routes = tableRoutes
+  .select(['order', 'email', 'role'])
+  .where({'department': ['==', department]})
+  .sort([{order: true}]) //昇順でソート
+  .result(false);
+
+  return routes;
+}
+
 function getMaxApprovalStep(department) {
+  // role=承認のレコード数を取得
   const result = tableRoutes
   .select(['role'])
   .where({
