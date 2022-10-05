382d92d3aa892b122b0c92d14e6138c7d2392d3c
yukimasaki <woodenhoe@gmail.com>
2022-10-05 05:04:56 +0000

    createEmailBody関数をMail.gsへ移動

--- Mail.js
+++ Mail.js
@@ -9,3 +9,19 @@ function addApprovalLink(id, token) {
 
   return approvalLink;
 }
+
+function createEmailBody(employee, formResponses) {
+  // 氏名、部署を書き込む
+  let emailBody =
+    '氏名：' + employee[0].name + '<br>' +
+    '部署：' + employee[0].department + '<br><br>'
+
+  // フォームの項目名、回答内容を書き込む
+  for (var i = 0, len = formResponses.questions.length; i < len; i++) {
+    emailBody = emailBody +
+    formResponses.questions[i] + '：<br>' +
+    formResponses.answers[i] + '<br><br>'
+  }
+
+  return emailBody;
+}
