d4998c57457bcb54153bca54436565e91260d08e
yukimasaki <woodenhoe@gmail.com>
2022-10-05 05:15:24 +0000

    getReaders関数をPaidLeaveRouteModel.gsへ移動

--- PaidLeaveRouteModel.js
+++ PaidLeaveRouteModel.js
@@ -34,3 +34,24 @@ function getCurrentApprover(id) {
 
   return approverEmail;
 }
+
+function getReaders(id) {
+  const department = tablePaidLeaves
+  .select(['department'])
+  .where({'id': ['==', id]})
+  .result(true);
+
+  const results = tableRoutes
+  .select(['email'])
+  .where({
+    'department': ['==', department],
+    'role': ['==', '回覧']
+  })
+  .result(true)
+
+  if (results == '') {
+    return false;
+  } else {
+    return results;
+  }
+}
