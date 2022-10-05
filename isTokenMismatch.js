a29d668a124763efb8ced9c4e4f286f41d6319b0
yukimasaki <woodenhoe@gmail.com>
2022-10-05 05:18:39 +0000

    isTokenMismatch.gsを削除

--- isTokenMismatch.js
+++
@@ -1,8 +0,0 @@
-function isTokenMismatch(id, paramToken) {
-  const latestApprover = tablePaidLeaves
-  .select(['token'])
-  .where({'id': ['==', id]})
-  .result(false);
-
-  return latestApprover[0].token != paramToken;
-}
