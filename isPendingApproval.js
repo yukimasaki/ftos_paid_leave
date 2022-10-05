389add0e33f4d6d8e7031fcd7a5eccd13a22bee9
yukimasaki <woodenhoe@gmail.com>
2022-10-05 01:44:47 +0000

    isPendingApproval関数から'id'を削除

--- isPendingApproval.js
+++ isPendingApproval.js
@@ -1,7 +1,7 @@
-function isPendingApproval(id) {
+function isPendingApproval(id_new) {
   const results = tablePaidLeaves
   .select(['current_approval_step', 'max_approval_step'])
-  .where({'id': ['==', id]})
+  .where({'id': ['==', id_new]})
   .result(false)
 
   const currentApprovalStep = results[0].current_approval_step;
